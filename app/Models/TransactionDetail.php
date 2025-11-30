b400cc24083624197bc48c0671ce943efd880b75
Rai-14 <sitarasmirasmi@gmail.com>
2025-11-30 18:22:14 +0800

    melengkapi isi file di folder app/Models

--- app/Models/TransactionDetail.php
+++ app/Models/TransactionDetail.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TransactionDetail extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = [
+        'transaction_id',
+        'product_id',
+        'quantity',
+    ];
+
+    /**
+     * Relasi ke Transaction
+     */
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+    
+    /**
+     * Relasi ke Product
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
