00b9ee8987712236b5c324943218497230093c73
Rai-14 <sitarasmirasmi@gmail.com>
2025-12-01 14:33:04 +0800

    Membuat file show.blade php di folder transactions dan memperbarui model RestockOrder.php serta RestockOrderDetail.php

--- app/Models/RestockOrderDetail.php
+++ app/Models/RestockOrderDetail.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class RestockOrderDetail extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = [
+        'restock_order_id',
+        'product_id',
+        'quantity',
+    ];
+
+    /**
+     * Relasi ke RestockOrder
+     */
+    public function restockOrder()
+    {
+        return $this->belongsTo(RestockOrder::class);
+    }
+    
+    /**
+     * Relasi ke Product
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
